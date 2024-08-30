bf85d90d7c5d87eb06160ba650764e06a45d9a80
NapolDeveloper <loma1016@gmail.com>
2024-08-30 10:14:15 +0900

    [Chore] 모노레포 세팅

---
+++ apps/landing/tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'tailwindcss';
+import sharedConfig from '@repo/tailwind-config';
+
+const config: Pick<Config, 'content' | 'presets'> = {
+  content: ['./src/app/**/*.tsx'],
+  presets: [sharedConfig],
+};
+export default config;
