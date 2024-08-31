8c4d863da7833f9e20e26e9b28321bed2dd9a10e
NapolDeveloper <loma1016@gmail.com>
2024-08-31 15:06:02 +0900

    [Chor] tailwind 에러 해결

---
+++ apps/landing/src/components/test.tsx
@@ -0,0 +1,3 @@
+export default function Test() {
+  return <div className="text-suldak-red-500">test</div>;
+}
--- apps/landing/tailwind.config.ts
+++ apps/landing/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import sharedConfig from '@repo/tailwind-config';
 
 const config: Pick<Config, 'content' | 'presets'> = {
-  content: ['./src/app/**/*.tsx'],
+  content: ['./src/app/**/*.tsx', './src/components/**/*.tsx'],
   presets: [sharedConfig],
 };
 export default config;
