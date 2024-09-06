37dde0fac1616b3986c6b807ef0f02b8e4ae5b0a
thisisthewa2 <thisisthewa2@gmail.com>
2024-09-06 18:40:37 +0900

    [Chore] 필요 라이브러리, 플러그인 설치

--- apps/landing/.eslintrc.js
+++ apps/landing/.eslintrc.js
@@ -13,4 +13,12 @@ module.exports = {
       },
     },
   },
+  'import/no-extraneous-dependencies': [
+    'error',
+    {
+      devDependencies: false,
+      optionalDependencies: false,
+      peerDependencies: false,
+    },
+  ],
 };
