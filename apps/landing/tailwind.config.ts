674a984963f73354811c556bba3d174736146224
thisisthewa2 <thisisthewa2@gmail.com>
2024-09-13 20:08:53 +0900

    [Chore] tailwind config 중복 코드 삭제

--- apps/landing/tailwind.config.ts
+++ apps/landing/tailwind.config.ts
@@ -6,51 +6,7 @@ const config: Partial<Config> = {
   presets: [sharedConfig],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors: {
-        'suldak-gray-900': '#1e1e1e',
-        'suldak-gray-700': '#646464',
-        'suldak-gray-600': '#8e8e8e',
-        'suldak-gray-500': '#bebebe',
-        'suldak-gray-400': '#e1e1e1',
-        'suldak-gray-300': '#e8e8e8',
-        'suldak-gray-200': '#f6f6f6',
-        'suldak-gray-100': '#f9f9f9',
-        'suldak-orange-500': '#ff9300',
-        'suldak-green-500': '#20bf27',
-        'suldak-red-500': '#e45141',
-        'suldak-mint-500': '#08beca',
-        'suldak-mint-50': '#f6fdfe',
-        'suldak-mint-100': '#CEF2F4',
-        'suldak-mint-200': '#9CE5EA',
-        'suldak-mint-600': '#0698A2',
-      },
-      boxShadow: {
-        'suldak-card': '2px 2px 20px -3px rgba(0, 0, 0, 0.13)',
-      },
-      padding: {
-        '18px': '18px',
-        '50px': '50px',
-      },
-      margin: {
-        '18px': '18px',
-      },
-      height: {
-        card: '140px',
-        'card-image': '90px',
-      },
-      width: {
-        'card-image': '90px',
-      },
-      minWidth: {
-        'card-image': '90px',
-      },
       fontFamily: {
-        pretendard: ['Pretendard', 'sans-serif'],
         GMarket: ['GMarket', 'sans-serif'],
       },
       screens: {
