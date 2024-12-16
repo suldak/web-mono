96affe96a851e141d8632a4a35e447816c23e567
thisisthewa2 <thisisthewa2@gmail.com>
2024-12-16 20:39:40 +0900

    Feat:팀원소개 페이지 구현

---
+++ apps/landing/src/app/our-crew/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import GlobalNavigationBar from '@/components/global-navigation-bar';
+import CrewDescriptHeader from '@/components/crew/crew-descript-header';
+import MascotSection from '@/components/crew/mascot-section';
+import CrewSection from '@/components/crew/crew-section';
+
+const OurCrewPage = (): JSX.Element | null => {
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="w-full flex flex-col items-center justify-center font-pretendard">
+      <GlobalNavigationBar />
+      <CrewDescriptHeader />
+      <MascotSection />
+      <CrewSection />
+    </div>
+  );
+};
+
+export default OurCrewPage;
