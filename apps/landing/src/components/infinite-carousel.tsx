0b2d4b9fbe762b20765b1eb3d2e653aadfd9d0d8
thisisthewa2 <thisisthewa2@gmail.com>
2024-12-19 16:43:55 +0900

    Style:캐러셀 이미지 비율 유지하게 변경

--- apps/landing/src/components/infinite-carousel.tsx
+++ apps/landing/src/components/infinite-carousel.tsx
@@ -61,11 +61,14 @@ function InfiniteCarousel({
             <Image
               alt={`Carousel item ${index + 1}`}
               src={item}
-              layout="responsive"
-              objectFit="cover"
               width={itemWidth}
               height={itemHeight}
-              loading="lazy"
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+                objectPosition: 'center',
+              }}
               placeholder="blur"
             />
           </div>
